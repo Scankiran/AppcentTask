428c1e0eca5bd8248726bb7889c8bb236d6bf62e
Said <cank32cank@gmail.com>
2020-11-20 12:40:31 +0300

    Game Detail  Back button dark mode bug fixed.

--- AppcentTask/Views/GameDetail/GameDetailViewComponents.swift
+++ AppcentTask/Views/GameDetail/GameDetailViewComponents.swift
@@ -14,7 +14,7 @@ extension GameDetailViewController {
         backButton1.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(backButton1)
         backButton1.setTitle("Back", for: .normal)
-        backButton1.setTitleColor(UIColor.black, for: .normal)
+        backButton1.setTitleColor(UIColor.label, for: .normal)
         (isFavorited) ? self.favoriteButton.setImage(UIImage.init(named: "heart_filled"), for: .normal):self.favoriteButton.setImage(UIImage.init(named: "heart"), for: .normal)
         backButton1.addTarget(self, action: #selector(backButton(_:)), for: .allTouchEvents)
         backButton1.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 32).isActive = true
